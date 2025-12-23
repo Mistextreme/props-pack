0525afefe75bd2bacdcb383702c201bc3cace424
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-23 17:16:18 +0000

    Folder with props - fxmanifest.lua

---
+++ edibles_pack/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'bbs'
+description 'All in 1 Edibles pack!'
+
+files {
+    'stream/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+
+dependency '/assetpacks'
