04aa1c415010dcbe4da23aba41d20d8d3ee711e8
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-23 16:38:03 +0000

    Upload: Animations/fxmanifest.lua

---
+++ Animations/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'bbs'
+description 'Animations'
+
+files {
+    'stream/*.ycd'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ycd'
+
+dependency '/assetpacks'
